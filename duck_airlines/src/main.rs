1ad5dc8c8dff4feb4fd165efeb30b7cf41fac67f
etch1000 <110674620+etch1000@users.noreply.github.com>
2022-08-17 16:41:26 +0530

    Refactored: duck airlines

--- duck_airlines/src/main.rs
+++ duck_airlines/src/main.rs
@@ -1,71 +1,52 @@
 // DUCK AIRLINES
 
-fn main() {
-    // Haversine : To measure distance between two coordinates
-    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
+struct Waypoint {
+    name: String,
+    latitude: f32,
+    longitude: f32,
+}
 
-    let route = [
-        ("KCLE", 41.4075, -81.851111),
-        ("LEYIR", 41.51030, -83.88080),
-        ("PIONS", 41.65390, -84.48190),
-        ("ZOSER", 41.72390, -84.78130),
-        ("MODEM", 41.72800, -84.89730),
-        ("BRYTO", 41.74170, -85.31320),
-        ("SEWTO", 41.74780, -85.51130),
-        ("GIJ", 41.76860, -86.31850),
-        ("NEPTS", 41.96750, -87.05300),
-        ("THORR", 42.12330, -87.60030),
-        ("OBK", 42.22140, -87.95160),
-        ("COTON", 42.31990, -89.31220),
-        ("DBQ", 42.40150, -90.70910),
-        ("VIGGR", 42.55520, -93.12410),
-        ("FOD", 42.61110, -93.29480),
-        ("ONL", 42.47050, -98.68690),
-        ("BFF", 41.89420, -103.48200),
-        ("OCS", 41.59020, -109.01500),
-        ("PUDVY", 41.54270, -109.34200),
-        ("WEGEM", 41.44560, -109.99000),
-        ("KSLC", 40.7861, -111.9822),
-    ];
+struct Segment {
+    start: Waypoint,
+    end: Waypoint,
+}
 
-    let mut total_distance = 0.0;
-
-    let mut previous_waypoint: Option<(&str, f64, f64)> = None;
-
-    for waypoint in route.iter() {
-        match previous_waypoint {
-            None => {
-                previous_waypoint = Option::from(waypoint.clone());
-                continue;
-            }
-            Some(previous_waypoint_value) => {
-                let previous_waypoint_radians = previous_waypoint_value.1.to_radians();
-                let waypoint_radians = waypoint.1.to_radians();
-
-                let delta_latitude = (previous_waypoint_value.1 - waypoint.1).to_radians();
-                let delta_longitude = (previous_waypoint_value.2 - waypoint.2).to_radians();
-
-                let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2)
-                    + previous_waypoint_radians.cos()
-                        * waypoint_radians.cos()
-                        * f64::powi((delta_longitude / 2.0).sin(), 2);
-
-                let central_angle = 2.0 * inner_central_angle.sqrt().asin();
-                let distance = EARTH_RADIUS_IN_KILOMETERS * central_angle;
-
-                total_distance += distance;
-                previous_waypoint = Option::from(waypoint.clone());
-
-                println!(
-                    "The distance between {} and {} is {:.1}",
-                    previous_waypoint_value.0, waypoint.0, distance
-                );
-            }
-        }
+impl Segment {
+    // Associated methods
+    fn new(start: Waypoint, end: Waypoint) -> Self {
+        Self { start, end }
     }
 
-    println!(
-        "Total distance between KCLE and KSLC is: {:.1}",
-        total_distance
-    );
+    fn distance(&self) -> f32 {
+        const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
+        let start_radians = self.start.latitude.to_radians();
+        let end_radians = self.end.latitude.to_radians();
+        let delta_latitude = (self.start.latitude - self.end.latitude).to_radians();
+        let delta_longitude = (self.start.longitude - self.end.longitude).to_radians();
+        let inner_central_angle = f32::powi((delta_latitude / 2.0).sin(), 2)
+            + start_radians.cos() * end_radians.cos() * f32::powi((delta_longitude / 2.0).sin(), 2);
+        let central_angle = 2.0 * inner_central_angle.sqrt().asin();
+        let distance = EARTH_RADIUS_IN_KILOMETERS as f32 * central_angle;
+        distance as f32
+    }
+}
+
+fn main() {
+    // Haversine : To measure distance between two coordinates
+
+    let kcle = Waypoint {
+        name: "KCLE".to_string(),
+        latitude: 41.4075,
+        longitude: -81.851111,
+    };
+
+    let kslc = Waypoint {
+        name: "KSLC".to_string(),
+        latitude: 40.7861,
+        longitude: -111.9822,
+    };
+
+    let kcle_kslc = Segment::new(kcle, kslc);
+    let distance = kcle_kslc.distance();
+    println!("Distance: {:.1}", distance);
 }
